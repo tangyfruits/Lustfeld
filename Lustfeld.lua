8d912a27955b8a5ab0565c36cdc7e497edff6e7e
Donald Tang <tangyfruits@gmail.com>
2016-07-09 03:52:28 +1200

    Created a table and included Alliance lusts and Drums of Fury

--- Lustfeld.lua
+++ Lustfeld.lua
@@ -3,8 +3,11 @@ Lust_EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 Lust_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
     local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...;
-    if ((args13 == "Bloodlust" and args2 == "SPELL_AURA_APPLIED") or (args13 == "Time Warp" and args2 == "SPELL_AURA_APPLIED")) then
-    Lust_EventFrame:BLOODLUST();
+    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury"};
+    for i, v in ipairs(spells) do
+      if(args2 == "SPELL_AURA_APPLIED" and args13 == v) then
+        Lust_EventFrame:BLOODLUST();
+      end
     end
 	end)
 
