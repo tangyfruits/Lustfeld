1d354f7f20bde2c71a216e23bb0a7480ab2b7e1a
Donald Tang <tangyfruits@gmail.com>
2016-07-09 15:50:52 +1200

    Removed Legacy of the White Tiger

--- Lustfeld.lua
+++ Lustfeld.lua
@@ -3,7 +3,7 @@ Lust_EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 Lust_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
     local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...
-    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury", "Legacy of the White Tiger"}
+    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury"}
     for i, v in ipairs(spells) do
       if(args2 == "SPELL_AURA_APPLIED" and args9 == UnitName("Player") and args13 == v) then
         Lust_EventFrame:BLOODLUST()
