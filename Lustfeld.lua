424f4ddaa4ab0b582e1ac34c134b8530b38ea300
Donald Tang <tangyfruits@gmail.com>
2016-07-09 15:50:03 +1200

    Fixed a bug where it played for other people's lust

--- Lustfeld.lua
+++ Lustfeld.lua
@@ -2,21 +2,20 @@ local Lust_EventFrame = CreateFrame("Frame")
 Lust_EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 Lust_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
-    local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...;
-    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury"};
+    local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...
+    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury", "Legacy of the White Tiger"}
     for i, v in ipairs(spells) do
-      if(args2 == "SPELL_AURA_APPLIED" and args13 == v) then
-        Lust_EventFrame:BLOODLUST();
+      if(args2 == "SPELL_AURA_APPLIED" and args9 == UnitName("Player") and args13 == v) then
+        Lust_EventFrame:BLOODLUST()
       end
     end
     for i, v in ipairs(spells) do
-      if(args2 == "SPELL_AURA_REMOVED" and args13 == v) then
-        print("got here");
-        StopSound(soundHandle, 2000);
+      if(args2 == "SPELL_AURA_REMOVED" and args9 == UnitName("Player") and args13 == v) then
+        StopSound(soundHandle, 2000)
       end
     end
 	end)
 
 function Lust_EventFrame:BLOODLUST()
-  willPlay, soundHandle = PlaySoundFile("Interface\\AddOns\\Lustfeld\\Seinfeld Theme.mp3");
+  willPlay, soundHandle = PlaySoundFile("Interface\\AddOns\\Lustfeld\\Seinfeld Theme.mp3")
 end
