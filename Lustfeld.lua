c0ca02a57a4bade07ff1912f625cd8eda6c24360
Donald Tang <tangyfruits@gmail.com>
2016-07-09 13:31:34 +1200

    Found and removed println

--- Lustfeld.lua
+++ Lustfeld.lua
@@ -4,7 +4,6 @@ Lust_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
     local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...;
     spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury"};
-    print(tostringall(...));
     for i, v in ipairs(spells) do
       if(args2 == "SPELL_AURA_APPLIED" and args13 == v) then
         Lust_EventFrame:BLOODLUST();
