105cdf506c772f3b4fe629b5185f05837b4c17ec
Donald Tang <tangyfruits@gmail.com>
2016-07-09 13:23:37 +1200

    Removed debugging code

--- Lustfeld.lua
+++ Lustfeld.lua
@@ -3,7 +3,7 @@ Lust_EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 Lust_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
     local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12, args13 = ...;
-    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury", "Legacy of the White Tiger"};
+    spells = {"Bloodlust", "Time Warp", "Heroism", "Ancient Hysteria", "Netherwinds", "Drums of Fury"};
     print(tostringall(...));
     for i, v in ipairs(spells) do
       if(args2 == "SPELL_AURA_APPLIED" and args13 == v) then
